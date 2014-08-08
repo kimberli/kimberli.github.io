8752f4199262dd4501ba7e61eab31ca0389783ea
kimberli <kimberlizhong@gmail.com>
2014-08-08 01:15:48 -0700

    More lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 function adjust() {
 	if( $(this).width() <= 1024 ) {
         $('#content').remove().insertAfter($('#visual'));
+        $('.border-path').attr('d','M0 0 L' + $('#content').innerWidth() + ' 0 Z');
     }
     else {
     	$('#visual').remove().insertAfter($('#content'));
+    	$('.border-path').attr('d','M0 0 L0 ' + $('#content').innerHeight() + ' Z');
     }
 
  	var divHeight = $('#content-box').innerHeight()/2;
