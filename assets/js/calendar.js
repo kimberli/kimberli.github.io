41423990c15153616c8d3bc63dbee8977bdffd0d
kimberli <kimberlizhong@gmail.com>
2015-09-26 17:49:19 -0400

    add google calendar rendering & date range limiting

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,72 +1,66 @@
 $(document).ready(function() {
 		
-		$('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,basicWeek,basicDay'
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,basicWeek'
+		},
+		allDay: true,
+		displayEventEnd: true,
+		editable: false,
+		eventLimit: 5, // allow "more" link when too many events
+		fixedWeekCount: false,
+		googleCalendarApiKey: 'AIzaSyCtq5y1rGdrlNnqDs1n1GBavHy4lDstckM',
+		height: 'auto',
+		weekends: false, 
+		eventSources: [
+			{
+				googleCalendarId: 'kimberlizhong@gmail.com',
+				className: 'calendar1',
+			}, 
+			{
+				googleCalendarId: 've8r280thumd5h4u5jqg69c1to@group.calendar.google.com',
+				className: 'calendar1',
 			},
-			weekends: false, 
-			defaultDate: Date(),
-			height: 'auto',
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2015-09-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2015-09-07',
-					end: '2015-09-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-09-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-09-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2015-09-11',
-					end: '2015-09-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-09-12T10:30:00',
-					end: '2015-09-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2015-09-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-09-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2015-09-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2015-09-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2015-09-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2015-09-28'
-				}
-			]
-		});
-		
+			{
+				googleCalendarId: '498kovu11pdglistkeac14k61o@group.calendar.google.com',
+				className: 'calendar3',
+			},
+			{
+				googleCalendarId: 'iec62ftusfqn2jk1phv465s29o@group.calendar.google.com',
+				className: 'calendar2',
+			},
+			{
+				googleCalendarId: '4cj5594qdd3c48beoopmpfeih4@group.calendar.google.com',
+				className: 'calendar2',
+			}
+		],
+		viewRender: function(currentView){
+			var minDate = moment().startOf('month');
+			while (minDate.day() < 2 || minDate.day() > 4) {
+			    minDate.add(1, 'day');
+			}
+			var maxDate = moment().add(1,'month').endOf('month');
+			while (maxDate.day() < 2 || maxDate.day() > 4) {
+			    maxDate.subtract(1, 'day');
+			}
+			if (minDate.isAfter(currentView.start) && minDate.isBefore(currentView.end)) {
+				$(".fc-prev-button").prop('disabled', true); 
+				$(".fc-prev-button").addClass('fc-state-disabled'); 
+			}
+			else {
+				$(".fc-prev-button").removeClass('fc-state-disabled'); 
+				$(".fc-prev-button").prop('disabled', false); 
+			}
+			if (maxDate.isAfter(currentView.start) && maxDate.isBefore(currentView.end)) {
+				$(".fc-next-button").prop('disabled', true); 
+				$(".fc-next-button").addClass('fc-state-disabled'); 
+			} else {
+				$(".fc-next-button").removeClass('fc-state-disabled'); 
+				$(".fc-next-button").prop('disabled', false); 
+			}
+		}
 	});
+	
+});
