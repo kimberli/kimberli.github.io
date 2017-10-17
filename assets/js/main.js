c938a380b75b8b72e3cd10c49af21fa3faa572b3
kimberli <kimberlizhong@gmail.com>
2017-10-17 03:53:53 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,18 @@ var data = [
     {
         "command": "ls -1",
         "prompt": "~/info",
-        "response": "<a href='/resume'>resume</a><br>" + 
+        "response": "<a href='mailto:kimberlizhong+site@gmail.com'>email</a><br>" + 
+                    "<a href='/resume'>resume</a><br>" + 
                     "<a href='/github'>github</a><br>" + 
                     "<a href='/linkedin'>linkedin</a><br>" + 
-                    "<a href='/medium'>medium</a>"
-    }
+                    "<a href='/medium'>medium</a><br>" + 
+                    "about_me.txt"
+    },
+    {
+        "command": "cat about_me.txt",
+        "prompt": "~/info",
+        "response": "My hobbies include Snapchatting low-quality food pics, playing with my puppy Java, and enjoying nature (in light doses). I'm always taking reading suggestions, and I'll offer you mine: I love reading newsletters like The New York Times' Interpreter and Matt Levine's Money Stuff. Hit me up via your medium of choice--I don't bite!"
+    },
 ];
 
 function createLevel() {
@@ -51,8 +58,24 @@ function typeCommand(index) {
             showResponse(index);
             addPrompt(index + 1);
             if (index + 1 === data.length) {
+                var response = $("<div class='response'></div>");
+                $(".code").append(response);
                 var field = $("<input class='input' id='user-prompt'></input>");
-                $("#command-" + data.length).append(field);
+                var submit = $("<input class='hidden' type='submit'></input>");
+                var form = $("<form></form>");
+                form.append(field);
+                form.append(submit);
+                form.submit(function(e) {
+                    e.preventDefault();
+                    if (field.val() === "rm -rf /") {
+                        response.text("u hacked me bye");
+                        field.prop("disabled", true);
+                    } else if (field.val() && !response.text()) {
+                        response.text("Jokes on you! This doesn't do anything ;)");
+                        field.prop("disabled", true);
+                    }
+                });
+                $("#command-" + data.length).append(form);
                 field.focus();
             } else {
                 setTimeout(function() {
@@ -66,14 +89,13 @@ function typeCommand(index) {
 function showResponse(index) {
     if (data[index].response) {
         var response = $("<div class='response'></div>");
-        response.html(data[index].response);
+        response.html("<p>" + data[index].response + "</p>");
         $(".code").append(response);
     }
 }
 
 $(document).ready(function() {
+    var responded = false;
     addPrompt(0);
     typeCommand(0);
-    setTimeout(function() {
-    }, 1000);
 });
